fc85e10ffc35ad8d972ffe9e889d92b2b7925842
tantrungnguyen2001 <trungnt@zigvy.com>
2023-05-08 06:24:20 +0700

    update training2

---
+++ Training2/src/task2.1/src/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from 'react'
+import './App.css'
+import Modal from './Component/Modal/Modal';
+
+class App extends Component{
+  render(){
+    return (
+      <>
+        <div className='training__title'>Training 2 - Task 2.1</div>
+        <Modal />
+      </>
+    )
+  }
+}
+
+export default App
---
+++ Training2/src/task2.1/src/Component/Modal/Modal.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import './Modal.css'
+
+export default class Modal extends Component {
+  state = {
+    show: false
+  }
+
+  handleShow = () => {
+    this.setState(prevState => {
+      return {
+        show: prevState.show === false ? true : false
+      }
+    })
+  }
+  render(){
+    return (
+      <>
+      <button className="myBtn" onClick={this.handleShow}>Open Modal</button>
+
+      {this.state.show ? 
+        <div className="modal">
+          <div className='modal__background'>
+            <div className="modal__content">
+              <p>Some text in the Modal..</p>
+              <button className="close" onClick={this.handleShow}>&times;</button>
+            </div>
+          </div>
+        </div>
+      : <></>}
+      </>
+    )
+  }
+}
