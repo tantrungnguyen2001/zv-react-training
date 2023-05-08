b5969061760567116868f76bcfb4541431f7ffb2
tantrungnguyen2001 <trungnt@zigvy.com>
2023-05-08 13:08:33 +0700

    edit task2

--- Training2/src/task2/src/Component/Modal/Modal.jsx
+++ Training2/src/task2/src/Component/Modal/Modal.jsx
@@ -4,18 +4,18 @@ import CloseButton from "../CloseButton/CloseButton"
 import Button from "../Button/Button"
 
 export default function Modal(){
-    const [modal, setModal] = useState(false)
+    const [isOpenModal, setIsOpenModal] = useState(false)
 
     const toggleModal = () => {
-      setModal(!modal)
+      setIsOpenModal(!isOpenModal)
     }  
 
     return (
         <>
         <Button toggle1={toggleModal}/>
     
-        {modal && (
-            <div id='modal' >
+        {isOpenModal && (
+            <div id='isOpenModal' >
                 <div className="modal__content">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias pariatur consectetur natus reiciendis autem illum asperiores, ratione delectus nihil perspiciatis ad odio fugit impedit animi aut consequuntur recusandae nobis deserunt cupiditate vel sequi neque eos error ullam. Minus aspernatur possimus eius beatae ullam quod, impedit, quisquam ratione aliquid amet eligendi.</p>
                     
