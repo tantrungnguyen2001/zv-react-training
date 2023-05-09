606dd202a9c8a49e4e92fb1d121d851cfc1a42b3
tantrungnguyen2001 <trungnt@zigvy.com>
2023-05-09 11:42:51 +0700

    update training4 task2

--- Training4/training4.2/src/App.jsx
+++ Training4/training4.2/src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {getCountries()}, [])
   
+  const getCountries = () => {
+    axios.get('https://restcountries.com/v3.1/all')
+      .then(data => setCountries(data.data))
+      .then(err => console.log(err))
+      .finally()
+  }
+
   return (
     <>
-      
+    <ul>
+      {countries.map((country, index) => (
+        <li key={index}>
+        <div>
+          {country.name.common}<br></br>
+          {country.name.official}
+        </div>
+        <br></br>
+        </li>
+      ))}
+    </ul>
     </>
   )
 }
