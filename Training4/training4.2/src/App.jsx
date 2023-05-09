64d1d067b1919653c110a252c423b64f77545df6
tantrungnguyen2001 <trungnt@zigvy.com>
2023-05-09 17:43:24 +0700

    update training4 task2

--- Training4/training4.2/src/App.jsx
+++ Training4/training4.2/src/App.jsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
+import { debounce } from 'lodash'
 
 function App() {
   const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState([])
 
   useEffect(() => {getCountries()}, [])
   
   const getCountries = () => {
     axios.get('https://restcountries.com/v3.1/all')
-      .then(data => setCountries(data.data))
+      .then(data => {
+        setCountries(data.data)
+        setSearch(data.data)
+      })
       .then(err => console.log(err))
       .finally()
   }
 
+  const filter = (e) => {
+    setSearch(countries.filter(country => country.name.common.toLowerCase().includes(e.target.value)))
+    // debounce(fff, 1000)
+  }
+
   return (
     <>
+    <input 
+      onChange={filter}
+    />
     <ul>
-      {countries.map((country, index) => (
+      {search.map((country, index) => (
         <li key={index}>
           <div>
             {country.name.common}<br></br>
