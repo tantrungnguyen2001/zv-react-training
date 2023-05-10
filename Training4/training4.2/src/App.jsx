dc8b047303e0b7eba163df1c25aa697f9a80b32e
tantrungnguyen2001 <trungnt@zigvy.com>
2023-05-10 10:13:25 +0700

    upload training4 task2

--- Training4/training4.2/src/App.jsx
+++ Training4/training4.2/src/App.jsx
@@ -1,45 +1,51 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
-import { debounce } from 'lodash'
+import { debounce, throttle } from 'lodash'
 
 function App() {
   const [countries, setCountries] = useState([])
-  const [search, setSearch] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {getCountries()}, [])
   
   const getCountries = () => {
-    axios.get('https://restcountries.com/v3.1/all')
-      .then(data => {
-        setCountries(data.data)
-        setSearch(data.data)
-      })
+    axios.get(`https://restcountries.com/v3.1/all`)
+      .then(data => {setCountries(data.data)})
       .then(err => console.log(err))
       .finally()
   }
 
-  const filter = (e) => {
-    setSearch(countries.filter(country => country.name.common.toLowerCase().includes(e.target.value)))
-    // debounce(fff, 1000)
+  const filterCountries = countries.filter(
+    country => {
+      return country.name.common.toLowerCase().includes(search.toLowerCase())
+    })
+
+  const updateSearchBarDebounce = e => {
+    setSearch(e?.target?.value)
+    console.log('Debounce Tick');
   }
 
+  const updateSearchBarThrottle = e => {
+    setSearch(e?.target?.value)
+    console.log('Throttle Tick');
+  }
+
+  const debounceOnChange = debounce(updateSearchBarDebounce, 200)
+  const throttleOnChange = throttle(updateSearchBarThrottle, 200)
+
   return (
-    <>
-    <input 
-      onChange={filter}
-    />
-    <ul>
-      {search.map((country, index) => (
-        <li key={index}>
-          <div>
-            {country.name.common}<br></br>
-            <img src={country.flags.png}/>
-          </div>
-        </li>
-      ))}
-    </ul>
-    </>
+    <div className='app'>
+      <input onChange={debounceOnChange} placeholder='Debounce...' style={{marginBottom: '8px'}}/>
+      <input onChange={throttleOnChange} placeholder='Throttle...'/>
+      <ul>
+        {filterCountries.map((country, index) => (
+          <li key={index}>
+            <div>{country.name.common}<br/></div>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
